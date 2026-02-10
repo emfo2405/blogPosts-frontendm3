6aa9c6a36ca1e99658b99d63f0679d035a1ac6df
emfo2405 <emfo2405@student.miun.se>
2026-02-10 07:40:33 +0100

    Modell för bloggpost skapat

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to='media/', blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.title
