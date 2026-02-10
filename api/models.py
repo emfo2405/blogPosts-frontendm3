0d8ab62f9d8d5b8fd1dfe89c3b2aee82a3797004
emfo2405 <emfo2405@student.miun.se>
2026-02-10 08:55:27 +0100

    Fel i databas fixade

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
-    text = models.TextField(blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
     image = models.ImageField(upload_to='media/', blank=True, null=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
--- blogPosts/settings.py
+++ blogPosts/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'blogPosts.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
