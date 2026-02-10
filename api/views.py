8ea9f8ccd2488e496c86a680b27193986370bd9f
emfo2405 <emfo2405@student.miun.se>
2026-02-10 09:22:03 +0100

    Autentisering tillagd för post

--- api/views.py
+++ api/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
