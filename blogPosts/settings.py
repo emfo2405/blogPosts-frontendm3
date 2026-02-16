5c0d80d90103a99ce8a8c4911c96f6b6436f1f19
emfo2405 <emfo2405@student.miun.se>
2026-02-16 13:47:35 +0100

    Felstavning fixad

--- blogPosts/settings.py
+++ blogPosts/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middlewear.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
