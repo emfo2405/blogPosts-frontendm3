a813b3ba969b0904f946aa063a4d470e50e95505
emfo2405 <emfo2405@student.miun.se>
2026-02-18 11:28:04 +0100

    Felstavning fixad

--- blogPosts/urls.py
+++ blogPosts/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_rot=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
